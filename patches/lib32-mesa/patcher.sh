3e1b4ef529660235fd9721af8cbee73bb113a7e2
Bouke Sybren Haarsma <boukehaarsma23@gmail.com>
2024-02-15 15:23:41 +0100

    update

--- patches/lib32-mesa/patcher.sh
+++ patches/lib32-mesa/patcher.sh
@@ -4,20 +4,22 @@ set -e
 set -x
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cp -rv $1/$2 $3/$2-chimeraos
+location=pkgs-stage-2
+rm -rf $location/$2-chimeraos
+cp -rv $1/$2 $location/$2-chimeraos
 # Suffix package with -chimeraos
-sed -i '/^pkgbase=/s/$/-chimeraos/' $3/$2-chimeraos/PKGBUILD
+sed -i '/^pkgbase=/s/$/-chimeraos/' $location/$2-chimeraos/PKGBUILD
 # In the prepare function replace
 # echo "$pkgver-arch$epoch.$pkgrel" >VERSION
-sed -i '/.*echo \"\$pkgver.*/s/arch/chos/' $3/$2-chimeraos/PKGBUILD
+sed -i '/.*echo \"\$pkgver.*/s/arch/chos/' $location/$2-chimeraos/PKGBUILD
 
 # Add patches to sources
 # https://stackoverflow.com/a/52057667
 for patch in $SCRIPT_DIR/*.patch
 do
     patchname=${patch##*/}
-    sed -i '/^source.*/{:a;/)/!{N;ba};s/)/'${patchname}'\n)\n/;}' $3/$2-chimeraos/PKGBUILD
+    sed -i '/^source.*/{:a;/)/!{N;ba};s/)/'${patchname}'\n)\n/;}' $location/$2-chimeraos/PKGBUILD
 done
-pushd "$3/$2-chimeraos"
+pushd "$location/$2-chimeraos"
 # plumb patches in folder
 cp -v $SCRIPT_DIR/*.patch .
