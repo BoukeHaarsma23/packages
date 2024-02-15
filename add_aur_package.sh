d6cf1ff4d020a24db1d9325385770958c308169c
Bouke Sybren Haarsma <boukehaarsma23@gmail.com>
2024-02-15 18:21:29 +0100

    update scripts [no ci]

--- add_aur_package.sh
+++ add_aur_package.sh
@@ -1,3 +1,3 @@
-pushd pkgs
+pushd $2
 	git submodule add "https://aur.archlinux.org/$1.git"
 popd
--- add_package.sh
+++ add_package.sh
@@ -1,3 +1,3 @@
-pushd pkgs
+pushd $2
 	git submodule add "https://gitlab.archlinux.org/archlinux/packaging/packages/$1.git"
 popd
